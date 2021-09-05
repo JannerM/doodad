2a69ff4e414adf7374671b8b751f1011fed9f1d9
janner <janner@mit.edu>
2021-09-05 05:06:52 -0700

    setup

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ setup(
     name='doodad',
     version='0.3.0dev',
     packages=find_packages(),
-    # scripts=['doodad/darchive/makeself.sh'],
-    # package_data={'': ['doodad/darchive/makeself.sh']},
     include_package_data=True,
     license='MIT License',
     long_description=open('README.md').read(),
