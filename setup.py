77cdefb9862625bb6f419e66cd502318225dc992
janner <jannermichael@gmail.com>
2022-05-01 21:36:21 -0700

    include requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='doodad',
     version='0.3.0dev',
     packages=find_packages(),
     include_package_data=True,
+    install_requires=required,
     license='MIT License',
     long_description=open('README.md').read(),
 )
